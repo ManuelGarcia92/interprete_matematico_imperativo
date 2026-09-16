e224b527a0f1ace950bcf225ba639164f601951d
ManuelGarcia92 <jmgb7738@gmail.com>
2026-09-16 14:23:27 -0400

    Cambios menores

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
+from extras import limpiar_terminal, pausa
 from tabla_de_simbolos import tabla_de_simbolos
 from lexer import tokenizar
 from parser import parsear
-from interprete import recorrer
+from evaluador import recorrer
 
-def limpiar_terminal() -> None:
-    import os
-    os.system("cls" if os.name == "nt" else "clear")
-
-
-while True:
-    limpiar_terminal()
-    print("[Ingrese xyz para salir.]")
-    texto = input(">>> : ")
-
-    if texto == "xyz":
-        break
-
-    try:
-        tokens = tokenizar(texto)
-        arbol = parsear(tokens)
-        memoria = tabla_de_simbolos()
-        resultado = recorrer(arbol, memoria, imprimir=True)
-        if resultado:
-            print(resultado)
-        else:
-            print()
-
-    except Exception as error:
-        print(error)
-
-    input("Presione ENTER para continuar...")
+def main() -> None:
+    while True:
+        limpiar_terminal()
+        print("[Ingrese break para salir]")
+        texto = input(">>>: ")
+        if texto == "break":
+            break
+        try:
+            tokens = tokenizar(texto)
+            arbol = parsear(tokens)
+            memoria = tabla_de_simbolos()
+            resultado = recorrer(arbol, memoria, imprimir=True)
+            if resultado:
+                print(resultado)
+            else:
+                print()
+        except Exception as error:
+            print(error)
+        pausa()
+    
+if __name__ == "__main__":
+    main()
